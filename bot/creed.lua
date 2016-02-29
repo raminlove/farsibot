d7f0cbafec91165c23b6e60085becb0d2937e8ee
mehdi <mehdizare863@yahoo.com>
2016-03-01 01:20:19 +0330

    Update creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -234,7 +234,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {137791771-137832653},--Sudo users
+    sudo_users = {137832653},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Creed bot 2.3
